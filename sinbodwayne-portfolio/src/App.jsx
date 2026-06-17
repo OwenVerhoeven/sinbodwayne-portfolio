8fb53edc072f4ad3ecf1f1977907b1ce68275449
Owen Verhoeven <owenverhoeven@protonmail.com>
2026-06-18 00:30:49 +0200

    Initial website

---
+++ sinbodwayne-portfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+      <div className="container">
+        <h1>Sinbod Wayne</h1>
+        <p>Videographer • Storyteller • Creator</p>
+      </div>
+  )
+}
+
+export default App
